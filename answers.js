384a95c1ed96bc4e0f1dcfb0c4f77c8eec64135f
Eve Papineau <eve.papineau@hotmail.fr>
2016-10-04 18:05:15 +0000

    added answers

--- answers.js
+++ answers.js
@@ -18,3 +18,31 @@ function character(string, number) {
 
 console.log(character("Eve", 2));
 
+function number(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(number(2,3));
+
+function number(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(number(2, 4));
+
+function math(num1, num2, string) {
+    switch(string) {
+        case 'add':
+            return num1 + num2;
+        case 'substract':
+            return num1 - num2;
+        case 'mult':
+            return num1 * num2;
+        case 'div':
+            return num1 / num2;
+        default:
+            return 0;
+    }
+};
+
+console.log(math(3, 2, "add"));
